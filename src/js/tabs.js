02054b50751c90368dfdda1aff59b9baafbff784
IronVeil <iron.veil@outlook.com>
2022-01-19 17:39:44 +0000

    Updated JS

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,19 +1,32 @@
-var tab1content = document.getElementById("tab1");
-var tab2content = document.getElementById("tab2");
+var tab1link = document.getElementById("tab1");
+var tab1content = document.getElementById("content1");
+var tab2link = document.getElementById("tab2");
+var tab2content = document.getElementById("content2");
 var currentTab = 1;
 
-function hide() {
+function hide(ifFirst) {
     document.querySelectorAll('.tab').forEach(div => {
         div.classList.add("hidden");
     });
+
+    document.querySelectorAll('.link').forEach(a => {
+        a.classList.remove("current");
+    })
+
+    if (ifFirst == 1) {
+        tab1content.classList.remove("hidden");
+        tab1link.classList.add("current");
+    }
 }
 
 function selectTab(tabNumber) {
     if (tabNumber == 1) {
         hide();
         tab1content.classList.remove("hidden");
+        tab1link.classList.add("current");
     } else if (tabNumber == 2) {
         hide();
         tab2content.classList.remove("hidden");
+        tab2.classList.add("current");
     }
 }
